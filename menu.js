d4f453c8dad9885368d02564515e98af60398075
Gloire Irenge <37586807+GloireIrenge@users.noreply.github.com>
2022-07-18 22:37:22 +0200

    Fix js linters

--- menu.js
+++ menu.js
@@ -4,22 +4,21 @@ let isOpen = false;
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
-  navMenu.style.display ='block';
-  document.body.style.overflow ='auto';
+  navMenu.style.display = 'block';
+  document.body.style.overflow = 'auto';
 });
-navMenu.addEventListener('click', ()=>{
-  navMenu.style.display ='none';
-  if(isOpen){
+navMenu.addEventListener('click', () => {
+  navMenu.style.display = 'none';
+  if (isOpen) {
     hamburger.classList.toggle('active');
     isOpen = false;
   } else {
     hamburger.classList.remove('active');
     isOpen = true;
   }
-})
+});
 
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
-
 }));
