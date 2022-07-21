d76f7d240006748fefa574381c72abf2f15fc3cb
Gloire Irenge <37586807+GloireIrenge@users.noreply.github.com>
2022-07-21 11:25:16 +0200

    store data from form input

---
+++ JS/localStorage_input.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('.form-contact');
+const dataStore = {
+    fullname: form.elements.fullname.value,
+    email: form.elements.email.value,
+    comment: form.elements.comment.value,
+  };
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const data = JSON.parse(localStorage.getItem('contact-info', dataStore));
+  if (data) {
+    fullname.value = data.fullname;
+    email.value = data.email;
+    comment.value = data.comment;
+  }
+});
+
+form.onchange = (event) => {
+  dataStore[event.target.id] = event.target.value;
+  localStorage.setItem('contact-info', JSON.stringify(dataStore));
+};
